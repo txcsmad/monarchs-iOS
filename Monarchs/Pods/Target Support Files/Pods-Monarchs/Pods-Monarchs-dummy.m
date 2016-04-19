56f0dd9226cf96e49d9cc3f58cb78ed7c87c6411
Mark Ruvalcaba <ruvalcaba.mark@gmail.com>
2016-04-19 00:00:05 -0500

    First Commit, general setup, added podfile w/ alamofire

---
+++ Monarchs/Pods/Target Support Files/Pods-Monarchs/Pods-Monarchs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Monarchs : NSObject
+@end
+@implementation PodsDummy_Pods_Monarchs
+@end
---
+++ Monarchs/Pods/Target Support Files/Pods-Monarchs/Pods-Monarchs-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MonarchsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MonarchsVersionString[];
+
