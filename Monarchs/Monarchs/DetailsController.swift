fa3c2585ba154c8164996b06689b62072435ce5f
Mark Ruvalcaba <ruvalcaba.mark@gmail.com>
2016-04-19 19:19:32 -0500

    Add comment for test

--- Monarchs/Monarchs/DetailsController.swift
+++ Monarchs/Monarchs/DetailsController.swift
@@ -18,6 +18,8 @@ class DetailsController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //Added comment
+        
     }
 
 
