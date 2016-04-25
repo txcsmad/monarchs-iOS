7492ab81ebee51ede4083c8d370abfc7cd2ec70f
Caroline Shi <detectiveconan123456789@gmail.com>
2016-04-25 16:57:07 -0500

    updated userflow

--- Monarchs/Monarchs/DetailsController.swift
+++ Monarchs/Monarchs/DetailsController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DetailsController: UIViewController {
 
+    @IBOutlet weak var pictureTaken: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
